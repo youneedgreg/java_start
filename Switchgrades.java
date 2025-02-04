002f4309f5e52d74e71fa787380338a0c2498204
youneedgreg <gregorytemwa1212@gmail.com>
2025-02-04 22:54:54 +0300

    switch grades

--- Switchgrades.java
+++ Switchgrades.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class Switchgrades {
-    
+    public static void main(String[] args) throws Exception{
+        System.out.println("Grade Score");
+        Scanner obj = new Scanner(System.in);
+        
+        System.out.println("Enter the score:");
+        int a = obj.nextInt();
+
+        
+        if (a > 100 || a < 0) {
+            System.out.println("Invalid score");
+            System.exit(0);
+        }
+
+        int grade = a / 10;
+
+        switch (grade) {
+            case 10, 9:
+                System.out.println(a + "Grade A");
+                break;
+            case 8:
+                System.out.println(a + "Grade B");
+                break;
+            case 7:
+                System.out.println(a + "Grade C");
+                break;
+            case 6:
+                System.out.println(a + "Grade D");
+                break;
+            default:
+                System.out.println(a + "Grade F");
+        }
+        //case 10, 9 -> System.out.println(x: "A")
+        obj.close();
+    }
 }
