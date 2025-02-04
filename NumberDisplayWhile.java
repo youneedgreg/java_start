c92f33b142b6439e6529326cea41b7f7f30ee402
youneedgreg <gregorytemwa1212@gmail.com>
2025-02-04 23:17:44 +0300

    fix

--- NumberDisplayWhile.java
+++ NumberDisplayWhile.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
 public class NumberDisplayWhile {
-
-    public static void main(String[] args) throws Exception{
+    int a;
+    int b;
+    public void getValues() {
+        Scanner obj = new Scanner(System.in);
         System.out.println("displaying numbers using while loop.");
         System.out.println("input two numbers first one smaller than the last.");
-        int a;
-        int b;
-        Scanner obj = new Scanner(System.in);
         System.out.println("Enter the first number:");
         a = obj.nextInt();
         System.out.println("Enter the second number:");
@@ -16,11 +15,18 @@ public class NumberDisplayWhile {
             System.out.println("Invalid input");
             System.exit(0);
         }
-        
+    }
+    public void performTask(){
         while (a <= b) {
             System.out.println(a);
             a++;
         }
+    }
+
+    public static void main(String[] args) throws Exception{
+        NumberDisplayWhile obj = new NumberDisplayWhile();
+        obj.getValues();
+        obj.performTask();
     }
     
 }
